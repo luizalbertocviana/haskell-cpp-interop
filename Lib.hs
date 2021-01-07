702dc6167b640f0e2c907951d15462c868df018f
Luiz Alberto <luizalbertocviana@gmail.com>
2021-01-07 10:59:22 -0300

    minor changes to export list and some signatures

--- Lib.hs
+++ Lib.hs
@@ -1,15 +1,13 @@
-module Lib where
+module Lib (withCPoint, getX, getY) where
 
 import Foreign.C.Types
 import Foreign.Ptr
 
 data CPoint = CPoint
 
-type CPointHandle = Ptr CPoint
+foreign import ccall "CPoint_new" newPoint :: CDouble -> CDouble -> IO (Ptr CPoint)
 
-foreign import ccall "CPoint_new" newPoint :: CDouble -> CDouble -> Ptr CPoint
-
-foreign import ccall "CPoint_delete" deletePoint :: Ptr CPoint -> ()
+foreign import ccall "CPoint_delete" deletePoint :: Ptr CPoint -> IO ()
 
 foreign import ccall "CPoint_x" getX :: Ptr CPoint -> CDouble 
 
